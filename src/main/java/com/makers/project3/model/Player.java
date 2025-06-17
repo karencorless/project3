2e374dc41157e6b1bfb250eb7272aa9a08a58805
Alexandre Barrymore <alexandrebarrymore@MASTUDENT-Alexandre-Barrymore-THFD966NGP.local>
2025-06-17 14:54:10 +0100

    final commit to branch, merging to main.

--- src/main/java/com/makers/project3/model/Player.java
+++ src/main/java/com/makers/project3/model/Player.java
@@ -1,11 +1,7 @@
 package com.makers.project3.model;
 
-import java.util.List;
-
 public class Player {
     private Integer playerUserId;
-    private Integer cardsInHand;
     private Integer currentScore;
     private Card currentCard;
-    private List<Card> discardPile;
 }
