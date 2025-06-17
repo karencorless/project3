6168ff4304f312d3adfc7a89522a5993b721d3fb
Alexandre Barrymore <alexandrebarrymore@MASTUDENT-Alexandre-Barrymore-THFD966NGP.local>
2025-06-17 11:17:52 +0100

    Finished creating initial models

---
+++ src/main/java/com/makers/project3/model/Game.java
@@ -0,0 +1,6 @@
+package com.makers.project3.model;
+
+public class Game {
+    private Player playerOne;
+    private Player playerTwo;
+}
---
+++ src/main/java/com/makers/project3/model/Player.java
@@ -0,0 +1,11 @@
+package com.makers.project3.model;
+
+import java.util.List;
+
+public class Player {
+    private Integer playerUserId;
+    private Integer cardsInHand;
+    private Integer currentScore;
+    private Card currentCard;
+    private List<Card> discardPile;
+}
