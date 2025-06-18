2d858698db11cc6a9488d932cfb63616d6d4681d
Karen <karencorless@yahoo.co.uk>
2025-06-18 15:00:16 +0100

    Update for game controller

--- src/main/java/com/makers/project3/controller/GameController.java
+++ src/main/java/com/makers/project3/controller/GameController.java
@@ -67,7 +67,6 @@ public class GameController {
         return new RedirectView("/game/play");
     }
 
-
 //    For displaying gameplay. More to add.
     @GetMapping("/game/play")
     public String playGame(Model model){
