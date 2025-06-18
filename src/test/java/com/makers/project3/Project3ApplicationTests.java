0f0455beaa4850d42c03ce9760fff37f4057029a
Barbora Srenkova <bara.srenkova@gmail.com>
2025-06-18 16:18:16 +0100

    CI- Test updated to have correct annotation

--- src/test/java/com/makers/project3/Project3ApplicationTests.java
+++ src/test/java/com/makers/project3/Project3ApplicationTests.java
@@ -2,8 +2,10 @@ package com.makers.project3;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootTest
+@Configuration
 class Project3ApplicationTests {
 
 	@Test
