8666416f81eb1ee8a5635901e6a417c32e1a572d
v 🪐 <vhrobertson05@gmail.com>
2025-06-19 12:56:32 +0100

    added tests for each of the models to ensure all values get saved correctly

---
+++ src/test/java/com/makers/project3/model/UserTest.java
@@ -0,0 +1,63 @@
+package com.makers.project3.model;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
+import java.util.Date;
+
+import org.junit.jupiter.api.Test;
+
+public class UserTest {
+    private User testUser = new User(1L, "test@test.com", "username", "pfp.jpg", new Date(1999, Calendar.DECEMBER, 31), 4, 10, "####abcd");
+
+    //    Tests that the user has an associated ID
+    @Test
+    public void userHasID() {
+        assertThat(testUser.getId(), equalTo(1L));
+    }
+
+    //    Tests that the user model saved the username correctly
+    @Test
+    public void userHasUsername() {
+        assertThat(testUser.getUsername(), equalTo("username"));
+    }
+
+    //    Tests that the user model saved the email correctly
+    @Test
+    public void userHasEmail() {
+        assertThat(testUser.getEmail(), equalTo("test@test.com"));
+    }
+
+    //    Tests that the user model saved the profile picture source name correctly
+    @Test
+    public void userHasPfp() {
+        assertThat(testUser.getImageSource(), equalTo("pfp.jpg"));
+    }
+
+    //    Tests that the user model saved the birthday correctly
+    @Test
+    public void userHasBirthday() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        assertThat(testUser.getBirthday(), equalTo(new Date(1999, Calendar.DECEMBER, 31)));
+    }
+
+    //    Tests that the user model saved the games won value correctly
+    @Test
+    public void userHasGamesWon() {
+        assertThat(testUser.getGamesWon(), equalTo(4));
+    }
+
+    //    Tests that the user model saved the games played value correctly
+    @Test
+    public void userHasGamesPlayed() {
+        assertThat(testUser.getGamesPlayed(), equalTo(10));
+    }
+
+    //    Tests that the user model saved the authenticator ID correctly
+    @Test
+    public void userHasAuthID() {
+        assertThat(testUser.getAuth0id(), equalTo("####abcd"));
+    }
+}
