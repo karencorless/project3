ee4d5d6ab52082004ce5687792ce2fa341eacbd8
Avian Schmigiel <avianschmigiel@gmail.com>
2025-06-24 01:51:42 +0100

    html, user, and game updates

--- src/main/java/com/makers/project3/controller/AfterLogin.java
+++ src/main/java/com/makers/project3/controller/AfterLogin.java
@@ -1,24 +1,48 @@
 package com.makers.project3.controller;
 
 import com.makers.project3.Service.AuthenticatedUserService;
+import com.makers.project3.Service.UserService;
 import com.makers.project3.model.User;
+import com.makers.project3.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.Banner;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
+
+import java.util.Date;
 
 @RestController
 public class AfterLogin {
 
     @Autowired
     AuthenticatedUserService authenticatedUserService;
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    UserService userService;
+
 
     @GetMapping("/users/after-login")
     public ModelAndView authenticateUserAndRedirect() {
         authenticatedUserService.getAuthenticatedUser();
         return new ModelAndView("redirect:/homepage");
     }
+
+    @GetMapping("")
+    public ModelAndView redirectEmpty() {
+        authenticatedUserService.getAuthenticatedUser();
+        return new ModelAndView("redirect:/homepage");
+    }
+
+    @GetMapping("/")
+    public ModelAndView redirectSlash() {
+        authenticatedUserService.getAuthenticatedUser();
+        return new ModelAndView("redirect:/homepage");
+    }
+
 
     @GetMapping("/homepage")
     public ModelAndView viewHomepage() {
@@ -27,7 +51,4 @@ public class AfterLogin {
         mav.addObject("currentUser", currentUser);
         return mav;
     }
-
-    @GetMapping("/settings")
-    public viewSettingsPage()
 }
